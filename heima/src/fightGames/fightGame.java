f07644f1bca5647a63ae95beea2ee154c5ac2c19
Mian <wangmian618@gmail.com>
2024-09-16 21:42:42 +0200

    V7

---
+++ heima/src/fightGames/fightGame.java
@@ -0,0 +1,32 @@
+package fightGames;
+
+public class fightGame {
+    public static void main(String[] args) {
+
+        Role qiao = new Role();
+        qiao.setName("乔峰");
+        qiao.setBlood(100);
+        qiao.setGender('男');
+
+
+        Role xiao = new Role("鸠摩智", 100, '女');
+
+        qiao.show();
+        xiao.show();
+
+        while (true) {
+            qiao.attack(xiao);
+            System.out.println(" ");
+            if (xiao.getBlood() <= 0) {
+                System.out.println(qiao.getName() + "获胜");
+                break;
+            }
+            xiao.attack(qiao);
+            System.out.println(" ");
+            if (qiao.getBlood() <= 0) {
+                System.out.println(xiao.getName() + "获胜");
+                break;
+            }
+        }
+    }
+}
