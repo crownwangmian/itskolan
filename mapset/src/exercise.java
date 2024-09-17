1f58bb5dbbce0d886844d9c48d41566417068638
Mian <wangmian618@gmail.com>
2024-09-17 14:34:14 +0200

    练习1

--- mapset/src/exercise.java
+++ mapset/src/exercise.java
@@ -2,53 +2,23 @@ import java.util.*;
 
 public class exercise {
     public static void main(String[] args) {
+        HashMap<Student, String> hm = new HashMap<Student, String>();
+        hm.put(new Student("Jonas", 20), "beijing");
+        hm.put(new Student("bob", 21), "shanghai");
+        hm.put(new Student("tom", 22), "wuhan");
+// keyset iterate
+        Set<Student> students = hm.keySet();
+        for (Student student : students) {
+            System.out.println(student.getName() + " " + student.getAge() + " " + hm.get(student));
+        }
+// foreach iterate
+        hm.forEach((k, v) -> System.out.println(k.getName() + " " + k.getAge() + " " + v));
 
-        Map<String, String> map = new HashMap<>();
-        map.put("yinzhiping", "xiaolongnv");
-        map.put("2", "xiaolongnv");
-        map.put("3", "xiaolongnv");
-
-        map.forEach((k, v) -> System.out.println(k + ":" + v));
-
-
-
-
-
-
-
-        // 通过键获得值
-//        Set<String> keys = map.keySet();
-//        // 迭代器
-//        Iterator<String> iterator = keys.iterator();
-////        while (iterator.hasNext()){
-////            String key = iterator.next();
-////            System.out.println(map.get(key));
-////        }
-////        // 增强for
-////        for (String key : keys) {
-////            System.out.println(map.get(key));
-////        }
-//        keys.forEach(s -> System.out.println(map.get(s)));
-
-//        获取键值对
-//        Set<Map.Entry<String, String>> entries = map.entrySet();
-//        //增强for
-//        for (Map.Entry<String, String> entry : entries) {
-//            System.out.println(entry.getKey()+entry.getValue());
-//        }
-//
-//        // foreach
-//        entries.forEach(entry -> System.out.println(entry.getKey()+entry.getValue()));
-//
-//        // 迭代器
-//        Iterator<Map.Entry<String, String>> iterator = entries.iterator();
-//        while (iterator.hasNext()) {
-//            Map.Entry<String, String> next = iterator.next();
-//            System.out.println(next.getKey()+next.getValue());
-//        }
-
-
-
+        // lambda
+        Set<Map.Entry<Student, String>> entries = hm.entrySet();
+        for (Map.Entry<Student, String> entry : entries) {
+            System.out.println(entry.getKey().getName() + " " + entry.getKey().getAge() + " " + entry.getValue());
+        }
 
 
     }
