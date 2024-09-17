ccafc4411bb3d5ef4e987b772b364d2ef7707872
Mian <wangmian618@gmail.com>
2024-09-17 15:12:28 +0200

    练习2

--- mapset/src/exercise.java
+++ mapset/src/exercise.java
@@ -2,24 +2,39 @@ import java.util.*;
 
 public class exercise {
     public static void main(String[] args) {
-        HashMap<Student, String> hm = new HashMap<Student, String>();
-        hm.put(new Student("Jonas", 20), "beijing");
-        hm.put(new Student("bob", 21), "shanghai");
-        hm.put(new Student("tom", 22), "wuhan");
-// keyset iterate
-        Set<Student> students = hm.keySet();
-        for (Student student : students) {
-            System.out.println(student.getName() + " " + student.getAge() + " " + hm.get(student));
-        }
-// foreach iterate
-        hm.forEach((k, v) -> System.out.println(k.getName() + " " + k.getAge() + " " + v));
+        Random r = new Random();
 
-        // lambda
-        Set<Map.Entry<Student, String>> entries = hm.entrySet();
-        for (Map.Entry<Student, String> entry : entries) {
-            System.out.println(entry.getKey().getName() + " " + entry.getKey().getAge() + " " + entry.getValue());
+
+        String[] arr = {"a", "b", "c", "d"};
+        ArrayList<String> record = new ArrayList<String>();
+        for (int i = 0; i < 80; i++) {
+            int ran = r.nextInt(arr.length);
+            record.add(arr[ran]);
+        }
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        for (String s : record) {
+            if (hm.containsKey(s)) {
+                hm.put(s, hm.get(s) + 1);
+            } else {
+                hm.put(s, 1);
+            }
         }
 
+        hm.forEach((k, v) -> System.out.println("tourist attraction " + k + "points " + v));
+
+        int count = 0;
+        String maxKey = null;
+
+        Set<Map.Entry<String, Integer>> entries = hm.entrySet();
+        for (Map.Entry<String, Integer> entry : entries) {
+            if (entry.getValue() > count) {
+                count = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+        System.out.println(maxKey + count);
 
     }
+
+
 }
